8235342847e8a64c46c28fccd174506897155fce
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-10-12 13:54:49 +0530

    imgur file

---
+++ main.js
@@ -0,0 +1,12 @@
+  // fetch('https://api.imgur.com/3/gallery/top/week/1?&album_previews=true', {
+  //   headers: {
+  //     Authorization: `Client-ID f9068c305c00fae`
+  //   }
+  // })
+  // .then(res=>{
+  //    return res.json()
+  // })
+  // .then(res=>{
+  //     console.log(res);
+  //     localStorage.setItem("data",JSON.stringify(res.data))
+  // })
